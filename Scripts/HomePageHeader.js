16bb0ccb8416fc3d4617c32ef980a8d361715cf4
StrungOwl <sydneymaeparks@gmail.com>
2024-07-01 18:35:57 -0500

    Ham Menu

--- Scripts/HomePageHeader.js
+++ Scripts/HomePageHeader.js
@@ -1,9 +1,6 @@
 function homeHeader() {
   //HEADER VIDEO -----------------
   let container = select("main");
-  let vidX = globeScale * 0.05;
-  let vidY = globeScale * 0.03;
-  vidSize = globeScale * 0.2;
 
   //parent vid to main html element so it shows at top of screen
   profileVid.parent(container);
@@ -23,14 +20,22 @@ function homeHeader() {
   });
 
   //HOME PAGE BUTTONS ----------------
-  // Create buttons based on buttonNames array
   const orangeColors = ["#F4A261", "#EE8959", "#EB7C55", "#E97653", "#E76F51"];
-
+  
   buttonNames.forEach((name, index) => {
+    
     let button = createButton(name);
-    let buttonSize = globeScale * 0.07;
-    let buttonY = vidY + buttonSize * 0.8;
-    let buttonX = (vidX + vidSize * 1.6) + index * (width * 0.1);
+
+    //DISPLAY THE BUTTONS HORZ OR VERT
+  if (!verticleButtonOn) {
+    buttonSize = globeScale * 0.07;
+    buttonY = vidY + buttonSize * 0.8;
+    buttonX = vidX + vidSize * 1.6 + index * (width * 0.1);
+  } else {
+    buttonSize = globeScale * 0.05;
+    buttonX = width * 0.9 - buttonSize * 0.8;
+    buttonY = height * 0.2 + index * (buttonSize * 1.2);
+  }
 
     button.size(buttonSize * 1.7, buttonSize); // Adjust size as
     button.position(buttonX, buttonY); // Adjust positioning as needed
@@ -38,7 +43,7 @@ function homeHeader() {
     button.style("font-size", `${fontSize}px`); // Change button text size here
     button.style("background-color", "white"); // Change button background color here
     button.style("font-family", "Space Grotesk"); // Change button text color here
-    button.style("box-shadow", "0px 8px 8px rgba(0, 0, 0, 0.5)"); // Add drop shadow
+    //button.style("box-shadow", "0px 8px 8px rgba(0, 0, 0, 0.5)"); // Add drop shadow
     buttons.push(button); // Add to buttons array for later use
 
     //color on mouseOver
@@ -72,11 +77,40 @@ function homeHeader() {
 }
 
 //HAM MENU -------------------
-const hamMenu = document.querySelector('.ham-menu');
-const offScreenMenu = document.querySelector('.off-screen-menu');
 
+class HamMenu {
+  constructor(x, y, size){
+    this.x = x;
+    this.y = y;
+    this.size = size;
 
-hamMenu.addEventListener('click', () => {
-    hamMenu.classList.toggle('active');
-    offScreenMenu.classList.toggle('active');
-})
+  }
+
+  displayHam(){
+    strokeCap(ROUND);
+    stroke(0);
+    strokeWeight(globeScale * 0.01);
+    line(this.x, this.y, this.x + this.size, this.y);
+    
+    strokeCap(SQUARE);
+    stroke(30);
+    strokeWeight(globeScale * 0.01);
+    line(this.x, this.y + this.size/2, this.x + this.size, this.y + this.size/2);
+    
+    strokeCap(ROUND);
+    stroke(50);
+    strokeWeight(globeScale * 0.01);
+    line(this.x, this.y + this.size, this.x + this.size, this.y + this.size);
+
+  }
+
+  interactHam(){
+   let d = dist(mouseX, mouseY, this.x, this.y);
+    if(d < this.size*1.5){
+        verticleButtonOn = true; 
+    } else {
+      verticleButtonOn = false;
+    }
+  }
+
+}
--- sketch.js
+++ sketch.js
@@ -5,21 +5,25 @@ let globeScale;
 
 //P5 Canvas
 let canvas1;
-let pg1; 
+let pg1;
 
 //CANVAS BACKGROUND
 let showTracer = false;
-let cursorHand; 
+let cursorHand;
 let dots = []; // Array to store Dots objects
-let ranFrameC; //random frame color 
+let ranFrameC; //random frame color
 let cRand; //random color for background
 let rectH; //top border
-let numShapes; 
+let numShapes;
 
 //HOME PAGE HEADER
-let profileVid, vidSize; //ai video of me
+let profileVid; //ai video of me
 let sydText; //Header
 let h1FontSize, h2FontSize; //Header font sizes
+let hamMenu1; //Hamburger menu
+let verticleButtonOn = false;
+let buttonX, buttonY, buttonSize;
+let vidX, vidY, vidSize;
 
 //BUTTONS ON HOME PAGE
 let buttons = [];
@@ -32,7 +36,6 @@ let buttonNames = [
   "About",
 ];
 
-
 function preload() {
   //PROFILE VIDEO ---------------------------
   profileVid = createVideo(["Video/AI Profile Pic.mp4"]);
@@ -40,20 +43,19 @@ function preload() {
 }
 
 function setup() {
-
   //CANVAS -------------------
   let w = windowWidth;
   let h = windowHeight;
-  canvas1 = createCanvas(w, h); //background canvas 
-  pg = createGraphics(w, h);  
+  canvas1 = createCanvas(w, h); //background canvas
+  pg = createGraphics(w, h);
   globeScale = min(width, height);
 
   //BACKGROUND CANAS
   colorMode(HSB);
-  imageMode(CENTER); 
+  imageMode(CENTER);
 
   //BACKGROUND -------------------
-  rectH = globeScale*0.25; 
+  rectH = globeScale * 0.25;
   cRand = color(random(255), random(255), random(255));
   // Create multiple Dots objects and add them to the array
   numShapes = random(10, 800);
@@ -61,26 +63,36 @@ function setup() {
     dots.push(new Dots());
   }
 
-  //PROFILE VID -------------------
-  homeHeader(); 
+  //PROFILE VID/Buttons -------------------
+  vidX = globeScale * 0.05;
+  vidY = globeScale * 0.03;
+  vidSize = globeScale * 0.2;
 
+  //HAM MENU -------------------
+  //x, y, size
+  let hamSize = globeScale * 0.05;
+  hamMenu1 = new HamMenu(width * 0.9 - hamSize / 2, height * 0.09, hamSize);
 }
 
 function draw() {
 
+  homeHeader();
+
   background(50, 0.1);
-  gradient(); 
 
-  homePageInteraction(); 
+  gradient();
 
-  if(cursorHand){
-    if(mouseY >= rectH){
-    noCursor(); 
-    image(cursorHand, mouseX, mouseY, globeScale*0.2, globeScale*0.2);
+  homePageInteraction();
+
+  hamMenu1.displayHam();
+  hamMenu1.interactHam();
+
+  if (cursorHand) {
+    if (mouseY >= rectH) {
+      noCursor();
+      image(cursorHand, mouseX, mouseY, globeScale * 0.2, globeScale * 0.2);
     } else {
-      cursor(ARROW); 
+      cursor(ARROW);
     }
   }
-  
-
 }
